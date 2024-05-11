5a52a60b62feb50983a2b925ef549ffd3855744c
OscarArCa <O.arias76510989@gmail.com>
2024-05-11 16:43:30 -0500

    Update Fuerza_de_espejo.java

--- gameOfCarts/src/com/gameOfCarts/models/Fuerza_de_espejo.java
+++ gameOfCarts/src/com/gameOfCarts/models/Fuerza_de_espejo.java
@@ -11,11 +11,5 @@ public class Fuerza_de_espejo extends Objeto{
             super("Trampa","FUERZA DE ESPEJO","Cuando un monstruo de un adversario declara un ataque: destruye todos los monstruos en Posición de Ataque.");
         }
 
-        public void Mostrar_datos(){
-            System.out.println("Tipo: "+getTipo());
-            System.out.println("Nombre: "+getNombre());
-            System.out.println("Detalle: "+getDescripcion());
-        }
-
 }
 
