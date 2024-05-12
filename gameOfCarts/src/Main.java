8094b8835a998fec1150b9523482348f709d246d
OscarArCa <o.arias76510989@gmail.com>
2024-05-12 15:32:52 -0500

    Soluciones el problema

--- gameOfCarts/src/Main.java
+++ gameOfCarts/src/Main.java
@@ -1,5 +1,8 @@
+import com.gameOfCarts.models.Fuerza_de_espejo;
+
 public class Main {
     public static void main(String[] args) {
-
+        Fuerza_de_espejo fe = new Fuerza_de_espejo();
+        fe.Mostrar_datos();
     }
 }
--- gameOfCarts/src/com/gameOfCarts/models/Objeto.java
+++ gameOfCarts/src/com/gameOfCarts/models/Objeto.java
@@ -1,5 +1,46 @@
 package com.gameOfCarts.models;
 
+import java.util.Scanner;
+
 public class Objeto {
+    private String tipo;
+    private String nombre;
+    private String descripcion;
+
+    public Objeto(String tipo,String nombre,String descripcion) {
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void Mostrar_datos(){
+        System.out.println("Tipo: "+getTipo());
+        System.out.println("Nombre: "+getNombre());
+        System.out.println("Detalle: "+getDescripcion());
+    }
 
 }
