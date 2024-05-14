eb308876eeed545dcd4bb1e191014d80203e6772
OscarArCa <O.arias76510989@gmail.com>
2024-05-14 14:09:56 -0500

    Update Objeto.java

--- gameOfCarts/src/com/gameOfCarts/models/Objeto.java
+++ gameOfCarts/src/com/gameOfCarts/models/Objeto.java
@@ -1,12 +1,15 @@
 package com.gameOfCarts.models;
 
-public class Objeto {
-private String tipo;
+
+public class Objeto implements Cartas{
+    private String tipo;
+    private String subtipo;
     private String nombre;
     private String descripcion;
 
-    public Objeto(String tipo,String nombre,String descripcion) {
+    public Objeto(String tipo,String subtipo,String nombre,String descripcion) {
         this.tipo = tipo;
+        this.subtipo=subtipo;
         this.nombre = nombre;
         this.descripcion = descripcion;
     }
@@ -17,6 +20,14 @@ private String tipo;
 
     public void setTipo(String tipo) {
         this.tipo = tipo;
+    }
+
+    public String getSubtipo() {
+        return subtipo;
+    }
+
+    public void setSubtipo(String subtipo) {
+        this.tipo = subtipo;
     }
 
     public String getNombre() {
@@ -35,9 +46,13 @@ private String tipo;
         this.descripcion = descripcion;
     }
 
+    //metodo encargado de mostrar los datos una vez hayan sido llenados.
     public void Mostrar_datos(){
-            System.out.println("Tipo: "+getTipo());
-            System.out.println("Nombre: "+getNombre());
-            System.out.println("Detalle: "+getDescripcion());
-        }
+        System.out.println("************************************************************************************************************************");
+        System.out.println("Tipo: "+getTipo()+"\n");
+        System.out.println("Subtipo: "+getSubtipo()+"\n");
+        System.out.println("Nombre: "+getNombre()+"\n");
+        System.out.println("Detalle: "+getDescripcion()+"\n");
+    }
+
 }
