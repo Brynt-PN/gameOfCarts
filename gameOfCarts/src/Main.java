7dcd6eb17bf2cd292ac5e6301b391447ab01031f
OscarArCa <O.arias76510989@gmail.com>
2024-05-14 14:06:53 -0500

    Update Main.java
    mostrar las cartas de ambas clases padre

--- gameOfCarts/src/Main.java
+++ gameOfCarts/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        //Metodo encargado de mostrar las cartas de ambas clases (Objeto y Criaturas) que esta en la clase Data_base.
+        Data_base db = new Data_base();
+        db.ShowData();
     }
 }
