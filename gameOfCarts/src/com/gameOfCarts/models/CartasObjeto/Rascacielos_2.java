36d7fd8da89b73ec9a3cd4f63996cdbcdd70c490
Brynt-PN <70989902bpn@gmail.com>
2024-05-16 18:21:20 -0500

    Corrección de herrores

--- gameOfCarts/src/com/gameOfCarts/models/CartasObjeto/Rascacielos_2.java
+++ gameOfCarts/src/com/gameOfCarts/models/CartasObjeto/Rascacielos_2.java
@@ -7,7 +7,6 @@ public class Rascacielos_2 extends Objeto {
 
     //Elimine el primer constructor por que no era necesario, y utilice comillas triples en la descripción
     public Rascacielos_2() {
-<<<<<<< HEAD
             super(
                     "MÁGICA",
                     "Campo",
@@ -20,19 +19,3 @@ public class Rascacielos_2 extends Objeto {
                             """);
     }
 }
-
-
-=======
-        super(
-                "MÁGICA",
-                "Campo",
-                "Rascacielos 2 - Ciudad de Héroes",
-                """
-                        Una vez por turno, durante tu Main Phase, si controlas esta carta:
-                        puedes seleccionar 1 monstruo HÉROE Elemental en tu Cementerio que
-                        fuera destruido en batalla y mandado ahí; Invoca ese objetivo
-                        de Modo Especial.
-                        """);
-    }
-}
->>>>>>> ab59dc8c047dd97bb3ea66e0ba8ed822fae9bb13
