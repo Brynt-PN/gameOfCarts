97a1728f9d86e5ffb00ccda98f89e155f8966fc6
Brynt-PN <70989902bpn@gmail.com>
2024-05-16 19:25:55 -0500

    Espero esta ves te fucnione Oscr

--- gameOfCarts/src/com/gameOfCarts/models/CartasCriatura/Archidemonio_Caballero_de_la_Sombra.java
+++ gameOfCarts/src/com/gameOfCarts/models/CartasCriatura/Archidemonio_Caballero_de_la_Sombra.java
@@ -1,4 +1,24 @@
 package com.gameOfCarts.models.CartasCriatura;
 
-public class Archidemonio_Caballero_de_la_Sombra {
+import com.gameOfCarts.models.Criaturas;
+
+public class Archidemonio_Caballero_de_la_Sombra  extends Criaturas {
+
+    public Archidemonio_Caballero_de_la_Sombra() {
+        super(
+                "VIENTO",
+                "Demonio / Efecto",
+                "Archidemonio Caballero de la Sombra",
+                4,
+                """
+                        Una vez por turno, durante tu Standby Phase, debes pagar 900 LP (esto no es opcional),
+                        o esta carta es destruida. Al resolver el efecto de una carta del adversario que
+                        seleccione esta carta, lanza un dado de seis caras y niega ese efecto si sale 3 y, si lo
+                        haces, destruye esa carta. Cualquier daño de batalla que esta carta inflige a tu
+                        adversario se divide a la mitad.
+                        """,
+                2000,
+                1600
+        );
+    }
 }
